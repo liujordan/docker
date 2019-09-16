9066118fd59bc0294d0d353cede87ebc8138e6a4
Jordan Liu <liu.jordan.com@gmail.com>
2019-09-16 19:32:10 -0400

    add copy tls files from env variables

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
-
+mkdir $HOME/.docker
+echo "$DOCKER_TLS_KEY" | base64 -d > $HOME/.docker/key.pem
+echo "$DOCKER_TLS_CA" | base64 -d > $HOME/.docker/ca.pem
+echo "$DOCKER_TLS_CERT" | base64 -d > $HOME/.docker/cert.pem
 sh -c "docker $*"
