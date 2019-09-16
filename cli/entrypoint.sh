2a7a793fbf21ecb74a9d4b4d97efe55e2ad9e044
Jordan Liu <liu.jordan.com@gmail.com>
2019-09-16 19:37:28 -0400

    replace hardcoded cert path with env var

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-mkdir $HOME/.docker
+mkdir $DOCKER_CERT_PATH
 echo "$DOCKER_TLS_KEY" | base64 -d > $HOME/.docker/key.pem
 echo "$DOCKER_TLS_CA" | base64 -d > $HOME/.docker/ca.pem
 echo "$DOCKER_TLS_CERT" | base64 -d > $HOME/.docker/cert.pem
