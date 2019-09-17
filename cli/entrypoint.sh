8b3737b938d588b9fa9de79c411a690cb4cab35c
Jordan Liu <liu.jordan.com@gmail.com>
2019-09-16 21:37:53 -0400

    rever to hard coded cert path

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
-mkdir -p $HOME/.docker
-export DOCKER_CERT_PATH=$HOME/.docker
-echo "$DOCKER_TLS_KEY" | base64 -d > $HOME/.docker/key.pem
-echo "$DOCKER_TLS_CA" | base64 -d > $HOME/.docker/ca.pem
-echo "$DOCKER_TLS_CERT" | base64 -d > $HOME/.docker/cert.pem
+mkdir -p /asdf/.docker
+export DOCKER_CERT_PATH=/asdf/.docker
+echo "$DOCKER_TLS_KEY" | base64 -d > /asdf/.docker/key.pem
+echo "$DOCKER_TLS_CA" | base64 -d > /asdf/.docker/ca.pem
+echo "$DOCKER_TLS_CERT" | base64 -d > /asdf/.docker/cert.pem
 sh -c "docker $*"
